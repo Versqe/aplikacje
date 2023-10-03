7f88ed65baabf1f26c25732bfa61002a3cfbb9b1
Versqe <weronika.chrzaszczed@gmail.com>
2023-10-03 08:54:15 +0200

    Data i czas w PHP

--- ogolne/dataiczas.php
+++ ogolne/dataiczas.php
@@ -10,3 +10,9 @@ CZAS
 <?php
     echo "The time is " . date("h:i:sa");
 ?>
+
+DATA Z GODZINĄ
+<?php
+    $d=mktime(11,14,51,8,12, 2014);
+    echo "Created date is " . date("Y-m-d h:i:sa", $d);
+?>

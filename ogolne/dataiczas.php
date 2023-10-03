1c405855869ea0f7f9279007846f327734b479f8
Versqe <weronika.chrzaszczed@gmail.com>
2023-10-03 08:34:57 +0200

    Data i czas w PHP

--- ogolne/dataiczas.php
+++ ogolne/dataiczas.php
@@ -1,6 +1,12 @@
+DATA
 <?php
     echo "Today is " . date("Y/m/d") . "<br>";
     echo "Today is " . date("Y.m.d") . "<br>";
     echo "Today is " . date("Y-m-d") . "<br>";
     echo "Today is " . date("l");
+?>
+
+CZAS
+<?php
+    echo "The time is " . date("h:i:sa");
 ?>

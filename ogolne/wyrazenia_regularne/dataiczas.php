2b9ef3a9f7e34ad1f4a3980342e2d0aedeee0611
abibasinho <adriankudzia1548@gmail.com>
2023-10-10 08:02:11 +0200

    Email w PHP

---
+++ ogolne/wyrazenia_regularne/dataiczas.php
@@ -0,0 +1,18 @@
+DATA
+<?php
+    echo "Today is " . date("Y/m/d") . "<br>";
+    echo "Today is " . date("Y.m.d") . "<br>";
+    echo "Today is " . date("Y-m-d") . "<br>";
+    echo "Today is " . date("l");
+?>
+
+CZAS
+<?php
+    echo "The time is " . date("h:i:sa");
+?>
+
+DATA Z GODZINĄ
+<?php
+    $d=mktime(11,14,51,8,12, 2014);
+    echo "Created date is " . date("Y-m-d h:i:sa", $d);
+?>
---
+++ ogolne/wyrazenia_regularne/email.php
@@ -0,0 +1,8 @@
+<?php
+    $wzorzec = '/[-\w.]+@([-A-z0-9]+\.)+[A-z]/';
+    $tekst = "nazwa.maila@gmail.com";
+    if(preg_match($wzorzec,$tekst))
+        echo "znaleziono email<br>";
+    else
+        echo "nie znaleziono emaila <br>";
+?>

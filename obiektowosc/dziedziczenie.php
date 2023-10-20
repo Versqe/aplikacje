c14964d2cb74f1b9548f08e7e999d5f498f3ee96
Versqe <weronika.chrzaszczed@gmail.com>
2023-10-20 11:53:49 +0200

    Dziedziczenie w klasach w PHP

--- obiektowosc/dziedziczenie.php
+++ obiektowosc/dziedziczenie.php
@@ -10,4 +10,12 @@
             echo "The fruit is {$this->name} and the color is {$this->color}.";
         }
     }
+    class Strawberry extends Fruit {
+        public function message() {
+            echo "Am I a fruit or a berry?";
+        }
+    }
+    $strawberry = new Strawberry("Strawberry", "red"); //OK _ _construct is public
+    $strawberry->message(); //OK message() is public
+    $strawberry->intro(); //ERROR intro() is protected
 ?>

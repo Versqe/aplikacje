25eccc7f795a3b4aaa9fdffa6dc46dd5f2167138
Versqe <weronika.chrzaszczed@gmail.com>
2023-10-20 13:09:26 +0200

    Dostep do bazy danych

--- mysql/dostep.php
+++ mysql/dostep.php
@@ -17,17 +17,11 @@
         echo "Error select database: " . mysqli_error($conn);
     }
 
-    $sql = "CREATE TABLE MyGuests (
-                    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                    firstname VARCHAR(30) NOT NULL,
-                    lastname VARCHAR(30) NOT NULL,
-                    email VARCHAR(50),
-                    reg_date TIMESTAMP DEFAULT
-                        CURRENT_TIMESTAMP ON UPDATE
-                        CURRENT_TIMESTAMP)";
-    if (mysqli_query($conn, $sql)) {
-        echo "Table MyGuests created successfully";
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+            VALUES ('John', 'Doe', 'john@example.com')";
+    if (mysqli_query($conn, $sql)){
+        echo "New record created successfully";
     } else {
-        echo "Error creating table: " . mysqli_error($conn);
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 ?>

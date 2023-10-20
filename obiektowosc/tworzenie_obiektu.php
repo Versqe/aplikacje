95ec3b469ef9c35ced5fe70063538453e0e37ec6
Versqe <weronika.chrzaszczed@gmail.com>
2023-10-20 11:15:30 +0200

    Obiekty w PHP

--- obiektowosc/tworzenie_obiektu.php
+++ obiektowosc/tworzenie_obiektu.php
@@ -39,3 +39,20 @@ class Fruit {
 $apple = new Fruit("Apple");
 echo $apple->get_name();
 ?>
+
+DESTRUKTOR
+<?php
+class Fruit {
+    public $name;
+    public $color;
+
+    function __construct($name){
+        $this->name = $name;
+    }
+    function __destruct() {
+        echo "The fruit is {$this->name}.";
+    }
+}
+
+$apple = new Fruit("Apple");
+?>

c1d8d8e6edb7ea41b6317ac57293c0db08a5341f
Versqe <weronika.chrzaszczed@gmail.com>
2023-10-20 13:00:23 +0200

    Dostep do bazy danych

--- mysql/dostep.php
+++ mysql/dostep.php
@@ -9,4 +9,25 @@
         die("Connection failed: " . mysqli_connect_error());
     }
     echo "Connected successfully";
+
+    $database = "5inb";
+    if (mysqli_select_db($conn, $database)) {
+        echo "Database $database selected";
+    }   else {
+        echo "Error select database: " . mysqli_error($conn);
+    }
+
+    $sql = "CREATE TABLE MyGuests (
+                    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    firstname VARCHAR(30) NOT NULL,
+                    lastname VARCHAR(30) NOT NULL,
+                    email VARCHAR(50),
+                    reg_date TIMESTAMP DEFAULT
+                        CURRENT_TIMESTAMP ON UPDATE
+                        CURRENT_TIMESTAMP)";
+    if (mysqli_query($conn, $sql)) {
+        echo "Table MyGuests created successfully";
+    } else {
+        echo "Error creating table: " . mysqli_error($conn);
+    }
 ?>

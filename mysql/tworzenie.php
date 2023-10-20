063187fcb96346dea174c1f39224e85db9656789
Versqe <weronika.chrzaszczed@gmail.com>
2023-10-20 13:11:43 +0200

    Tworzenie w bazie danych

---
+++ mysql/tworzenie.php
@@ -0,0 +1,41 @@
+TWORZENIE BAZY DANYCH
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+
+    $conn = mysqli_connect($servername, $username, $password);
+
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully";
+
+
+    $database = "5inb";
+    $sql = "CREATE DATABASE $database";
+    if (mysqli_query($conn, $sql)) {
+        echo "Database created successfully";
+    }   else {
+        echo "Error creating database: " . mysqli_error($conn);
+    }
+
+TWORZENIE TABELI
+$sql = "CREATE TABLE MyGuests (
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                firstname VARCHAR(30) NOT NULL,
+                lastname VARCHAR(30) NOT NULL,
+                email VARCHAR(50),
+                reg_date TIMESTAMP DEFAULT
+                    CURRENT_TIMESTAMP ON UPDATE
+                    CURRENT_TIMESTAMP)";
+if (mysqli_query($conn, $sql)) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+?>
+

6afea62f765e16afa6a57d5350bb2ca882b82424
Versqe <weronika.chrzaszczed@gmail.com>
2023-10-20 07:55:56 +0200

    Tworzenie obiektu w klasach w PHP

--- obiektowosc/tworzenie_obiektu.php
+++ obiektowosc/tworzenie_obiektu.php
@@ -10,4 +10,13 @@ class Fruit {
         return $this->name;
     }
 }
+
+$apple = new Fruit();
+$banana = new Fruit();
+$apple->set_name('Apple');
+$banana->set_name('Banana');
+
+echo $apple->get_name();
+echo "<br>";
+echo $banana->get_name();
 ?>
